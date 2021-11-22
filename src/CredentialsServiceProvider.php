f3138cb732add5c24958ad1083604bcc0cde80af
axel <dzhurko.a@gmail.com>
2021-11-22 14:21:46 +0200

    Debug

--- src/CredentialsServiceProvider.php
+++ src/CredentialsServiceProvider.php
@@ -65,7 +65,7 @@ class CredentialsServiceProvider extends ServiceProvider
             // If the key starts with "base64:", we will need to decode the key before handing
             // it off to the encrypter. Keys may be base-64 encoded for presentation and we
             // want to make sure to convert them back to the raw bytes before encrypting.
-            if (Str::startsWith($key = config('credentials.key'), 'base64:')) {
+            if (Str::startsWith($key, 'base64:')) {
                 $key = base64_decode(substr($key, 7));
             }
 
